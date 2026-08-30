a36116a5c46de9806ee68bbaf15ec45d6f4560a0
kxg0180-sudo <kxg0180@gmail.com>
2026-08-29 20:51:41 -0500

    downloaded plawright and tested commands

--- simple_scraper.py
+++ simple_scraper.py
@@ -1,6 +1,30 @@
 import time
 from playwright.sync_api import sync_playwright
+import re
+from playwright.sync_api import Page, expect
 #comment example
 #second comment example
 
+with sync_playwright() as p:
+   browser = p.chromium.launch(headless=False)
+   page = browser.new_page()
+   page.goto('https://google.com')
+   print(page.title())
+   browser.close()
 
+
+
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
